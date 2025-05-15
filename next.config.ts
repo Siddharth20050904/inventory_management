0d9d0c7490d0cd9af89edddb47211d09b48cc866
Siddharth20050904 <siddharthsuryavanshi0409@gmail.com>
2025-05-15 16:35:58 +0530

    error - 2

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    return config;
-  },
 };
 
 export default nextConfig;
